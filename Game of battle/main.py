1e6daa88966739a65e601c158266c2cb838c6041
JonathanMelancon <melancon1987@gmail.com>
2025-01-24 20:45:17 -0500

    implemented draw and value comparison. need to investigate why draw() called in loop before compare_values

--- Game of battle/main.py
+++ Game of battle/main.py
@@ -1,9 +1,10 @@
 import random
+import sys
 
 
-card_names:list[str] =["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", " Jack", "Queen", "King" ]
+card_names:list[str] =["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" ]
 card_values:list[int]= [14, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ,11, 12, 13]
-card_shapes:list[str] = ["Heart", "Spades", "Diamond", "Club"]
+card_shapes:list[str] = ["Hearts", "Spades", "Diamonds", "Clubs"]
 
 class Card:
 
@@ -25,31 +26,103 @@ def create_deck():
             i += 1
         i = 0
 
-    return(card_deck)
+    return card_deck
 
-card_deck = create_deck()
+new_deck = create_deck()
 
-def shuffle_deck(deck):
+def shuffle(card_deck):
     i = 0
     for _ in card_deck:
         rand = random.randrange(0,51)
         card_deck[i], card_deck[rand] = card_deck[rand], card_deck[i]
         i += 1
-    return(deck)
+    return card_deck
 
-card_deck = shuffle_deck(card_deck)
+starting_deck = shuffle(new_deck)
 
         
-# # class Player:
+class Player:
 
-# #     def __init__(self, name , cards):
-# #         self.name:str = name
-# #         self.cards:list = cards\
+    def __init__(self, name , cards):
+        self.name:str = name
+        self.cards:list = cards
 
-print("new")
+    def list_cards(self):
+        i = 0
+        for _ in self.cards:
+            print(self.cards[i].name + ' of ' + self.cards[i].shape)
+            i += 1
+    def card_description(self,i):
+        return self.cards[i].name + ' of ' + self.cards[i].shape
 
-for cards in card_deck:
-    print(cards.__dict__)
+    def card_count(self):
+        return len(self.cards)
+
+
+
+player1 = Player('Player 1', starting_deck[0:25])
+player2 = Player('Player 2', starting_deck[26:51])
+
+def draw():
+    if player1.card_count() > 0 and player2.card_count() > 0:
+        print('Player 1 plays ' + player1.card_description(0))
+        print('Player 2 plays ' + player2.card_description(0))
+        return [player1.cards[0], player2.cards[0]]
+
+def compare_values():
+    if draw()[0].value > draw()[1].value:
+        print(draw()[0].value , draw()[1].value)
+        print('Player 1 wins this round')
+        player1.cards.append(player2.cards.pop(0))
+        player1.cards.append(player1.cards.pop(0))
+
+    elif draw()[0].value < draw()[1].value:
+        print('Player 2 wins this round')
+        print(draw()[0].value, draw()[1].value)
+        player2.cards.append(player1.cards.pop(0))
+        player2.cards.append(player2.cards.pop(0))
+
+    elif draw()[0].value == draw()[1].value:
+        print('Battle! First player to draw a '+ draw()[0].name + ' wins the battle!')
+
+    else:
+        print(draw()[0].value, draw()[1].value)
+        print('wtf')
+
+    print('Player 1 has ' + str(player1.card_count()) + ' cards')
+    print('Player 2 has ' + str(player2.card_count()) + ' cards')
+
+def show_card_count(player):
+    print(player +' has ' + str(player.card_count()) + ' cards')
+
+def play_round():
+    draw()
+    compare_values()
+
+
+
+key = ''
+
+while key != 'e':
+
+    play_round()
+    if player1.card_count() < 52 and player2.card_count() < 52:
+        key = input('Press enter to draw next cards')
+
+    if key == 'e':
+        sys.exit()
+
+    elif player1.card_count() == 52:
+        print('Player one wins!')
+        sys.exit()
+
+    elif player2.card_count() == 52:
+        print('Player two wins!')
+        sys.exit()
+
+# print(player1.card_description())
+# player1.list_cards()
  
 
-print (len(card_deck))
+# print (len(card_deck))
+# print (player1.cards[1].name)
