b095a0d1bddf67dd79e4398e13d0db25a547babc
JonathanMelancon <195829876+JonathanMelancon@users.noreply.github.com>
2025-02-01 20:28:15 -0500

    implemented draw and value comparison. need to investigate why draw() called in loop before compare_values

--- Game of battle/main.py
+++ Game of battle/main.py
@@ -1,10 +1,13 @@
 import random
 import sys
 
+
 card_names: list[str] = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen",
                          "King"]
 card_values: list[int] = [14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 card_shapes: list[str] = ["Hearts", "Spades", "Diamonds", "Clubs"]
+
+
 
 
 class Card:
@@ -14,6 +17,9 @@ class Card:
         self.shape: str = shape
         self.value: int = value
         self.graphic = graphic
+
+    def card_description(self):
+        return str(self.name + ' of ' + self.shape)
 
 
 def create_deck():
@@ -45,7 +51,7 @@ def shuffle(card_deck):
 starting_deck = shuffle(new_deck)
 
 
-class Player:
+class CardPlayer:
 
     def __init__(self, name, cards):
         self.name: str = name
@@ -57,55 +63,92 @@ class Player:
             print(self.cards[i].name + ' of ' + self.cards[i].shape)
             i += 1
 
-    def card_description(self, i):
-        return self.cards[i].name + ' of ' + self.cards[i].shape
-
     def card_count(self):
         return len(self.cards)
 
+current_card:int = 0
+pile1:[Card] = []
+pile2:[Card] = []
+winner:CardPlayer
 
-player1 = Player('Player 1', starting_deck[0:25])
-player2 = Player('Player 2', starting_deck[26:51])
+player1 = CardPlayer('Player 1', starting_deck[0:25])
+player2 = CardPlayer('Player 2', starting_deck[26:51])
 
 
 def draw():
-    if player1.card_count() > 0 and player2.card_count() > 0:
-        print('Player 1 plays ' + player1.card_description(0))
-        print('Player 2 plays ' + player2.card_description(0))
-        return [player1.cards[0], player2.cards[0]]
+    global pile1
+    global pile2
+
+    pile1.insert(0,(player1.cards.pop(0)))
+    pile2.insert(0,(player2.cards.pop(0)))
 
 
-def compare_values():
-    if draw()[0].value > draw()[1].value:
-        print(draw()[0].value, draw()[1].value)
-        print('Player 1 wins this round')
-        player1.cards.append(player2.cards.pop(0))
-        player1.cards.append(player1.cards.pop(0))
+def show_card_description():
+    print('Player 1 plays ' + pile1[0].card_description())
+    print('Player 2 plays ' + pile2[0].card_description())
 
-    elif draw()[0].value < draw()[1].value:
-        print('Player 2 wins this round')
-        print(draw()[0].value, draw()[1].value)
-        player2.cards.append(player1.cards.pop(0))
-        player2.cards.append(player2.cards.pop(0))
 
-    elif draw()[0].value == draw()[1].value:
-        print('Battle! First player to draw a ' + draw()[0].name + ' wins the battle!')
+def battle():
+
+    global winner
+
+    if player1.card_count() <52 and player2.card_count()<52:
+
+        if pile1[0].value > pile2[0].value:
+            print('Player 1 wins this round')
+            winner = player1
+
+        elif pile1[0].value < pile2[0].value:
+            print('Player 2 wins this round')
+            winner = player2
+
+        elif pile1[0].value == pile2[0].value:
+            war()
+
+        else:
+            print('SOMETHING WENT WRONG')
+
 
     else:
-        print(draw()[0].value, draw()[1].value)
-        print('wtf')
-
-    print('Player 1 has ' + str(player1.card_count()) + ' cards')
-    print('Player 2 has ' + str(player2.card_count()) + ' cards')
+        print('Error, one player has no card left')
 
 
-def show_card_count(player):
-    print(player + ' has ' + str(player.card_count()) + ' cards')
+
+def swap_cards():
+
+    winner.cards.append(pile1)
+    winner.cards.append(pile2)
+    pile1.clear()
+    pile2.clear()
+
+
+def show_card_count():
+    print(player1.name + ' has ' + str(player1.card_count()) + ' cards')
+    print(player2.name + ' has ' + str(player2.card_count()) + ' cards')
+
+def war():
+    print('A war begins!')
+    draw()
+    print(pile1,pile2)
+    print('Player one places ' + pile1[0].card_description() + ' face down on the table')
+    print('Player two places ' + pile2[0].card_description() + ' face down on the table')
+    draw()
+    print(pile1,pile2)
+    print('Player one places ' + pile1[0].card_description() + ' face up on the table')
+    print('Player two places ' + pile2[0].card_description() + ' face up on the table')
+    battle()
+    swap_cards()
+    print(pile1,pile2)
+
 
 
 def play_round():
     draw()
-    compare_values()
+    show_card_description()
+    battle()
+    swap_cards()
+    show_card_count()
+
 
 
 key = ''
